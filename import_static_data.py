071adfeae0e24a41d574a352383ff4fc08dfcbbb
Mason <park.mason.w@gmail.com>
2019-05-04 23:23:34 -0700

    Connect to riot games api Download static data as tar archive file

---
+++ import_static_data.py
@@ -0,0 +1,53 @@
+import requests
+import sys
+import tarfile
+import traceback
+
+DATA_DRAGON_URL_HTTP = 'https://ddragon.leagueoflegends.com/cdn/dragontail-%s.tgz'
+LOCAL_FILENAME = 'data_dragon.tgz'
+
+def initialize_static_tables():
+    print('Initializing static data tables...')
+    patch_version = get_patch_manual();
+    success = get_data_dragon_tarfile(patch_version) # Load file. File will be accessible at LOCAL_FILENAME
+    if not success:
+        print('No tables were initialized. Exiting...')
+        return
+    print(tarfile.is_tarfile(LOCAL_FILENAME))
+
+def update_static_tables():
+    print('Updating static data tables...')
+    return None
+
+def get_patch_manual():
+    return input('Please enter desired patch version in X.Y.Z format.\n X=season, Y=major version, Z=minor version\n')
+
+# Return True if successful. False if not.
+def get_data_dragon_tarfile(patch_version):
+    url = DATA_DRAGON_URL_HTTP % patch_version
+    print('Retrieving tar file from Riot Games API: %s...' % url)
+    try:
+        with requests.get(url, timeout=(3, None), stream=True) as response:
+            response.raise_for_status()
+            total_length = float(response.headers.get('content-length'))
+            with open(LOCAL_FILENAME, "wb") as file:
+                # Iterate over chunks
+                length_written = 0
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk: # filter out keep-alive new chunks
+                        file.write(chunk)
+                        length_written += len(chunk)
+                        # If we have a content-length header, show progress bar
+                        if total_length is not None:
+                            sys.stdout.write("\r%d%" % int(length_written / total_length * 100))
+                            sys.stdout.flush()
+        print()
+        return True
+
+    except Exception as err:
+        print('Exception encountered when retrieving Riot Data Dragon tar file: %s', str(err))
+        traceback.print_tb(err.__traceback__)
+        return False
+
+if __name__ == '__main__':
+    initialize_static_tables()
